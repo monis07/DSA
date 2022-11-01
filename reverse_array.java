4a615715b3c18bedb1de08c6703ad71452a48c07
Monis Azeem <83574521+monis07@users.noreply.github.com>
2022-11-01 18:31:06 +0530

    Update reverse_array.java

--- reverse_array.java
+++ reverse_array.java
@@ -14,3 +14,32 @@ public class reverse_array {
         }
     }
 }
+// import java.util.*;
+// import java.lang.*;
+// import java.io.*;
+// class GFG
+//  {
+// 	public static void main (String[] args)
+// 	 {
+// 	 //code
+// 	 Scanner in=new Scanner(System.in);
+// 	 int t=in.nextInt();
+// 	 for(int i=1;i<=t;i++){
+// 	     int n=in.nextInt();
+// 	     int arr[]=new int[n];
+// 	     for(i=0;i<n;i++)
+// 	     arr[i]=in.nextInt();
+// 	     int low=0,high=n-1;
+// 	     while(low<high){
+// 	         int temp=arr[low];
+// 	         arr[low]=arr[high];
+// 	         arr[high]=temp;
+// 	         low++;
+// 	         high--;
+// 	     }
+// 	     for(i=0;i<n;i++)
+// 	     System.out.print(arr[i]+" ");
+// 	     System.out.println();
+// 	 }
+// 	 }
+// }
